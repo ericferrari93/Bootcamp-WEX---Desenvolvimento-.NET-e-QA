7536cd5be40d230c3bfd0d0d8fd0d99ada371086
Eric Ferrari Ramos <ericferrarir@hotmail.com>
2023-10-28 12:42:32 -0300

    Projeto de orientação a objetos, Abstraindo um celular com POO

---
+++ Projeto trilha .NET poo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Xml.Schema;
+using DesafioPOO.Models;
+
+// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+Smartphone cel1 = new Nokia(numero:"1198643-3290", modelo:"XX", imei:"1235", memoria:64);
+cel1.Ligar();
+cel1.InstalarAplicativo("Whatsapp");
+cel1.ReceberLigacao();
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone");
+Smartphone cel2 = new Iphone(numero:"1198643-3292", modelo:"XY", imei:"1236", memoria:32);
+cel2.Ligar();
+cel2.InstalarAplicativo("Waze");
+cel2.ReceberLigacao();
